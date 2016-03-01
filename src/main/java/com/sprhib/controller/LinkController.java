aaf337a1fe0c4d09f4c327570f86fffa7bf87ad5
Rajendra Botre <rajendra.botre@xarysma.com>
2016-03-01 17:23:53 +0530

    Added remaining project files

---
+++ src/main/java/com/sprhib/controller/LinkController.java
@@ -0,0 +1,24 @@
+package com.sprhib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LinkController {
+	
+	@RequestMapping(value="/")
+	public ModelAndView mainPage() {
+		return new ModelAndView("home");
+	}
+	
+	@RequestMapping(value="/index")
+	public ModelAndView indexPage() {
+		return new ModelAndView("home");
+	}
+
+    @RequestMapping(value="/gallery")
+    public ModelAndView galleryPage() {
+        return new ModelAndView("gallery");
+    }
+}
